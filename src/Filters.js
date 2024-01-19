342a5bfeae6ead241bcf79779fbdad9321845ae8
Kenneth Kebaara <kkebaara@yahoo.com>
2024-01-18 17:57:03 -0700

    filters update

--- src/Filters.js
+++ src/Filters.js
@@ -7,3 +7,25 @@
 }
 
 export default Filters; */
+
+function Filters({ greased, setGreased }) {
+	//const [greased, setGreased] = useState(false); moved to movieslist
+
+	function handleGreasedChanged() {
+		setGreased((currentGreased) => !currentGreased);
+	}
+
+	return (
+		<form>
+			<label htmlFor='greased'>Greased</label>
+			<input
+				id='greased'
+				type='checkbox' 
+				checked={greased}
+				onChange={handleGreasedChanged}
+			/>
+		</form>
+	);
+}
+
+export default Filters;
